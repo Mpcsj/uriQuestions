dec7833451ddfe6c1e3711acbbef58e05ecbe448
Mpcsj <marcosjunioribm@gmail.com>
2018-02-09 12:55:14 -0200

    Add files via upload

---
+++ 2662.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+class Nota:
+    def __init__(self,valor):
+        self.valor = valor
+
+
+
+dicionarioNotas = {0:'do',1:'do#',2:'re',3:'re#',4:'mi',5:'fa',6:'fa#',7:'sol',8:'sol#'
+                   ,9:'la',10:'la#',11:'si',12:'desafinado'}
+
+notasFundamentais = {0:0,2:2,4:4,5:5,7:7,9:9,11:11}
+n = int(input())
+incr = 0
+aux = 0
+notasTocadas= tuple(Nota(0) for i in range(n))
+
+for i in range(n):
+    notasTocadas[i].valor = (int(input())-1)%12 # ja saberei qual nota sera no intervalo fundamental
+
+
+while incr <12:
+    aux = incr
+    for el in notasTocadas:
+        if (el.valor-incr)% 12 not in notasFundamentais:
+            incr += 1
+            break
+    if aux == incr:
+        break
+
+print(dicionarioNotas[incr])
