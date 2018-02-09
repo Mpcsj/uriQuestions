7066e22004eb6f6baa83f650ec5183682281b311
Mpcsj <marcosjunioribm@gmail.com>
2018-02-09 14:19:59 -0200

    Update 2662.py

--- 2662.py
+++ 2662.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 
-'''
-Escreva a sua solução aqui
-Code your solution here
-Escriba su solución aquí
-'''
 class Nota:
     def __init__(self,valor):
         self.valor = valor
